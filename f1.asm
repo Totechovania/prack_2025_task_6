edb0a8f3a30515d79578e15f6e5b522f6e354572
vania <bi0686@gmail.com>
2025-05-10 13:47:53 +0300

    f1 optimized

--- f1.asm
+++ f1.asm
@@ -7,10 +7,9 @@ f1:
     finit
     fldl2e ; log_2 e
     fchs; log_2 e^-1
-    fmul qword[ebp + 8] ; log_2 e^-x
-    fld st0 ; log_2 e^-x : log_2 e^-x
+    fmul qword[ebp + 8] ; log_2 e^-x ; log_2 e^-x : log_2 e^-x
     fld1 ; 1 : log_2 e^-x : log_2 e^-x
-    fxch ; log_2 e^-x : 1 : log_2 e^-x
+    fld st1
     fprem ; {log_2 e^-x} : 1 : log_2 e^-x
     f2xm1 ; 2^{log_2 e^-x} - 1 : 1 : log_2 e^-x
     fadd ; 2^{log_2 e^-x} : log_2 e^-x
