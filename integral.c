ced9e49b84387c5dcab94e5a561a90fb95cf3d30
vania <bi0686@gmail.com>
2025-05-10 17:56:39 +0300

    integral fixed

--- integral.c
+++ integral.c
@@ -1,45 +1,44 @@
 #include <math.h>
 #include "integral.h"
-#include <stdio.h>
 
 double integral(double (*f)(double), double a, double b, double eps2){
-
-    double res_1 = f(a) + f(b);
-    res_1 /= 2;
-
     int n = 1;
-    double res_2 = res_1;
-    double h = (b - a);
-    int i = 0;
-    do {
+    double h = (b - a) / (2 * n);
 
-        if (i == 28){
-            printf("Warning: calculations are to heavy for given epsilon accuracy. Current accurecy is %lf.\n",
-                 2 * fabs(h * res_1 - h * res_2));
-            break;
-        }
+    double edges = f(a) + f(b);
+    double odd = 0;
+    double even = 0;
 
-        i++;
+    for (int i = 1; i < 2 * n; i += 2) {
+        odd += f(a + i * h);
+    }
+
+    for (int i = 2; i < 2 * n; i += 2) {
+        even += f(a + i * h);
+    }
+    
+    double res_2n = h * (edges + 2 * even + 4 * odd) / 3;
+    double res_n;
+
+    do
+    {
         n *= 2;
         h /= 2;
 
+        even += odd;
+        odd = 0;
 
-        double tmp;
-        tmp = res_1;
-        res_1 = res_2;
-        res_2 = tmp; 
-
-        res_1 *= 2;
-
-
-        for (int j = 1; j < n; j += 2) {
-            res_2 += f(a + h * j); 
-
+        for (int i = 1; i < 2 * n; i += 2) {
+            odd += f(a + i * h);
         }
 
-            
-    } while (fabs(h * res_1 - h * res_2) > eps2);
-    
+        printf("%lf\n", res_2n);
 
-    return res_2 * h;
+        res_n = res_2n;
+        res_2n = h * (edges + 2 * even + 4 * odd) / 3;
+
+    } while (fabs(res_2n - res_n) > eps2);
+    
+    
+    return res_2n;
 }
